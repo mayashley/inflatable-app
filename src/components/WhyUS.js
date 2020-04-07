60538b0c0a4767ae26ddf50443f1f0b080a455e2
Ashley <mayashley052@gmail.com>
2020-04-07 18:41:36 -0400

    updates the servies component to specific info

--- src/components/WhyUS.js
+++ src/components/WhyUS.js
@@ -1,34 +1,42 @@
-import React from 'react'
-import Title from '../constants/Title'
-import styles from '../css/whyUS.module.css'
-import services from '../constants/services'
+import React from "react";
+import Title from "../constants/Title";
+import styles from "../css/whyUS.module.css";
+import services from "../constants/services";
 
-
- const WhyUS = () => {
-    return (
-        <div className={styles.whyUsWrapper}>
-            <div className={styles.titleWrapper}>
-             <Title title="Why Choose Us?"/>
-             </div>
-        <div className={styles.services}>
-            <div className={styles.center}>
-                {
-                    services.map((item, index)=>{
-                        return <article key={index} className={styles.services}>
-                            <span>{item.icon}</span>
-                            <h4>{item.title}</h4>
-                            <p>{item.text}</p>
-                        </article>
-                    })
-                }
-            </div>
-            <div className={styles.btnWrapper}>
-            <button type="button" className="btn-primary">
+const WhyUS = () => {
+  return (
+    <div className={styles.whyUsWrapper}>
+      <div className={styles.titleWrapper}>
+        <Title title="Why Choose Us?" />
+      </div>
+      {/* <div className={styles.whyParagraph}>
+        <p>
+        At Southern Ohio Inflatables we believe life is all about creating
+          memories and it is our mission to provide our customers with the types
+          of experiences that will be remembered forever. When you choose us,
+          you are choosing a company that takes personal interest in your
+          event and ensuring that your special day is full of cheerful memories.
+        </p>
+      </div> */}
+      <div className={styles.services}>
+        <div className={styles.center}>
+          {services.map((item, index) => {
+            return (
+              <article key={index} className={styles.services}>
+                <span>{item.icon}</span>
+                <h4>{item.title}</h4>
+                <p>{item.text}</p>
+              </article>
+            );
+          })}
+        </div>
+        <div className={styles.btnWrapper}>
+          <button type="button" className="btn-primary">
             Have Questions?
           </button>
-          </div>
         </div>
-        </div>
-    )
-}
-export default WhyUS
+      </div>
+    </div>
+  );
+};
+export default WhyUS;
--- src/constants/services.js
+++ src/constants/services.js
@@ -5,31 +5,31 @@ export default [
   {
     icon: <GiAmericanShield />,
     title: "We are fully Insured",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Null",
+    text: "Southern Ohio Inflatables is fully insured with Cassio Insurance Agency. We carry both accidental and general liability insurance coverage.",
   },
   {
     icon: <GiThreeFriends/>,
-    title: "COURTEOUS AND FRIENDLY STAFF",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Null",
+    title: "COURTEOUS AND FRIENDLY",
+    text: "Our staff is courteous, friendly and professional throughout the whole process of planning your event.",
   },
   {
     icon: <GiCardExchange />,
     title: "Accomodating",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Null",
+    text: "We know how stressful planning an event can be and it is our goal to make that process easier for you.",
   },
   {
     icon: <GiFamilyHouse />,
     title: "Family Owned and Opperated",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Null",
+    text: "A locally family owned and operated business since 2017.",
   },
   {
     icon: <GiLaserSparks />,
     title: "Clean and Safe Equiptment",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Null",
+    text: "All of our equipment is sanitized and inspected before each use",
   },
   {
     icon: <GiSun />,
     title: "All Day Rental",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Null",
+    text: "All of our rentals are based on an 8 hour day. We do offer over nights and multiple day rentals for an additional fee.",
   },
 ]
