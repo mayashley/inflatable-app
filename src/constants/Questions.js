2fae7aad530b01c70db8a90666f3f92350e4a789
Ashley <mayashley052@gmail.com>
2020-08-07 13:19:00 -0400

    added all the faq's and clickable email and phone number for contact options

--- src/constants/Questions.js
+++ src/constants/Questions.js
@@ -1,60 +1,208 @@
-
-
-
 export default [
   {
-    section: "-Ordering",
+    section: "Ordering",
     title: "How soon should I book?",
-    text: " You should book your items as soon as possible to ensure availability, and remember that your item isn’t reserved until you have placed the deposit",
+    text:
+      " You should book your items as soon as possible to ensure availability, and remember that your item isn’t reserved until you have placed the deposit",
   },
   {
-   
     title: "How do I make a reservation?",
-    text: "By simply contacting us to ensure availability and then placing your deposit.",
+    text:
+      "By simply contacting us to ensure availability and then placing your deposit.",
   },
   {
-    
     title: "How do I receive a quote?",
-    text: "You can call or submit an interest through our website. Through our website, you will add the items you’re interested in to the quote and we will get back with you",
+    text:
+      "You can call or submit an interest through our website. Through our website, you will add the items you’re interested in to the quote and we will get back with you",
   },
   {
-    section:"-Payment",
+    section: "Payments",
     title: "What form of payment do you accept?",
-    text: "We take cash, money orders, checks, Visa, MasterCard, or Discovery. There will be a $75 returned check fee on all returned checks, No Exceptions. You can pay with card online or by phone.",
+    text:
+      "We take cash, money orders, checks, Visa, MasterCard, or Discovery. There will be a $75 returned check fee on all returned checks, No Exceptions. You can pay with card online or by phone.",
   },
   {
-   
     title: "When is final payment due?",
-    text: "All payments in cash or check are due and expected at time of delivery. If you are paying with credit, you must pay in full prior to delivery",
+    text:
+      "All payments in cash or check are due and expected at time of delivery. If you are paying with credit, you must pay in full prior to delivery",
   },
   {
-   
     title: " Do I have to pay a deposit to book my event",
-    text: "Yes we require a deposit of $75 per unit. A credit card of record is preferred for the depo    sit. Deposits made by credit card will be refunded within 2 business days of the end of the rental period. Deposits paid by check or cash will be mailed out within 2 business days of the end of the rental period. To ensure you receive your full deposit back, please adhere to all of our policies",
+    text:
+      "Yes we require a deposit of $75 per unit. A credit card of record is preferred for the depo    sit. Deposits made by credit card will be refunded within 2 business days of the end of the rental period. Deposits paid by check or cash will be mailed out within 2 business days of the end of the rental period. To ensure you receive your full deposit back, please adhere to all of our policies",
   },
   {
-   
     title: "Do you charge sales tax?",
-    text: "Yes, 7.25% sales tax will be added to all of our rental rates. If you are tax exempt, please inform us of this when you call to reserve your unit. We will need your tax exempt number.",
+    text:
+      "Yes, 7.25% sales tax will be added to all of our rental rates. If you are tax exempt, please inform us of this when you call to reserve your unit. We will need your tax exempt number.",
   },
   {
-   section:"-Inflatables",
+    section: "Inflatables",
     title: "Does the price include set up and delivery?",
-    text: "The first 15 miles is free, outside of our 15 mile delivery radius, it is a $1.00 per mile each way (Example: If you live 20 miles away, it would be an additional $10.00 delivery). Remember prices do not include sales tax",
+    text:
+      "The first 15 miles is free, outside of our 15 mile delivery radius, it is a $1.00 per mile each way (Example: If you live 20 miles away, it would be an additional $10.00 delivery). Remember prices do not include sales tax",
   },
   {
-   
     title: "Where are you located?",
     text: "We are located at 369 Foster Road, West Union, Ohio 45693",
   },
   {
-   
     title: "Does Southern Ohio Inflatables offer Customer Pick-ups?",
-    text: "Unfortunately, we cannot allow people who rent our inflatable to set them up due to our insurance. We have to set up our items so that we ensure they are safe, and set up to the standard provided by the Ohio Department of Agriculture.",
+    text:
+      "Unfortunately, we cannot allow people who rent our inflatable to set them up due to our insurance. We have to set up our items so that we ensure they are safe, and set up to the standard provided by the Ohio Department of Agriculture.",
   },
   {
     title: "How long does it take to set-up?",
-    text: "Most inflatables take approximately 30 minutes to set-up, larger units may take up to an hour.",
+    text:
+      "Most inflatables take approximately 30 minutes to set-up, larger units may take up to an hour.",
   },
-  
-]
+  {
+    title: "When should I expect delivery",
+    text:
+      " We provide a 3 hour window for delivery, with a call to confirm when we are 30  minutes away. Please be sure to provide the most convenient number for us to be able to contact you on the date of the delivery. A second delivery charge will be applied if you are unavailable to take delivery after a delivery window has been agreed upon. A responsible party must be there during this time to accept delivery.",
+  },
+  {
+    title: "Do you rent by the Hour?",
+    text:
+      "No, we take the setup and cleaning of our Bounce houses and Water slides very seriously. Therefore, we only rent our jumps and slides out once a day. This gives us time to make sure our inflatables are inspected, sanitized and properly installed. A FULL DAY IS CONSIDERED 8 HOURS. Although, earlier pickup times can be arranged",
+  },
+  {
+    title: "Do you offer a discount for renting multiple units?",
+    text: "Yes, we offer discounts for renting multiple items",
+  },
+  {
+    title: "Will the inflatables harm my lawn?",
+    text:
+      "We cannot guarantee that the inflatables will not affect your lawn. In most cases, it will only temporarily flatten the grass, which should recover in a few days.",
+  },
+  {
+    title: "How much area do I need?",
+    text:
+      "All Inflatables have measurements on our description page, you should add 10 feet to each measurement to have plenty room. (Don't forget height)",
+  },
+  {
+    title: "What is the capacity of the inflatable?",
+    text:
+      "Each inflatable is different and you can find this information in the description of the inflatable on our website.",
+  },
+  {
+    title: "What type of power is required for an inflatable?",
+    text:
+      "Our inflatable rides and games are inflated using a fully enclosed electric blower unit that is powered by a standard 110 volt, 20 AMP, three-prong electrical outlet. Each blower draws roughly 9-10 Amps (1,000-1,100w) and some larger units use multiple blowers. Electricity is needed within 75 feet of the inflatable. These motors run constantly to keep the unit inflated so constant ample power will be required. If a generator is needed, we do rent them at an additional price. Concession equipment should not be placed on the same power source. The units are powered by a standard 110 volt, 20 AMP, three-prong electrical outlet. Each machine requires between 7-11 Amps (900-1,300w)",
+  },
+  {
+    title: "Do I need to supply anything?",
+    text:
+      "Southern Ohio Inflatables will include everything needed to set up within 75 feet of a power supply/water supply. Please make sure there is a clean area for set-up, remove any branches, rocks or sharp objects. And Please make sure the lawn has been recently mown and raked. High grass and/or loose grass clippings make for a mess. If the lawn has not been maintained, we do reserve the right to cancel. Prior to delivery and pick up, please remove pet feces in and around the area where equipment is being setup and used. Please prevent pets from urinating on equipment while it is set up on your property.",
+  },
+  {
+    title: "Can I change the unit I rent?",
+    text:
+      "You may change your unit with a 7 day notice and granted there is availability.",
+  },
+  {
+    title: "What surfaces do you set up on?",
+    text:
+      "We can set up on Grass (our favorite and best for the kids), dirt, asphalt, and concrete. Sorry, we can ́t set up on any type of rocks as the constant rubbing will wear through the vinyl jumps. Please let us know as soon as possible if we will be setting up on concrete so we can make arrangements for proper anchoring of your unit. NOTE: We will not set up water slides on concrete, wet slides, kids and concrete do not mix.",
+  },
+  {
+    title: " Can the inflatables be set-up indoors?",
+    text:
+      "Yes, our units can be set up inside or outside. You will need to have enough ceiling height to accommodate the inflatable. The inflatable must not be set up near hot lights, fire prevention equipment, air conditioning vents, or any fire hazards",
+  },
+  {
+    title: "Can you set up in a park or other public location",
+    text:
+      " Yes, however, the customer is responsible for obtaining proper permission and/or permits for equipment rented and placed in parks or other public locations",
+  },
+  {
+    title: "Can we see a copy of your contract and safety rules?",
+    text: "Yes, please contact our office for a copy.",
+  },
+  {
+    title:
+      "Are we responsible for the unit if it gets a tear or damaged in any way",
+    text:
+      "Yes and no. You are not responsible for normal wear and tear on our units. Seams may develop tears in high traffic areas over a period of time. If this happens please alert us at once so we can remedy the situation. If however, damage occurs due to failure to follow our safety rules or negligence (i.e. not turning off the blower in high winds, sharp objects, silly string, adults using a bounce house not rated for adults) you will be responsible for all damages up to and including replacement of the unit/blower which can cost thousands of dollars. We don't want you or us to be in that situation which is why we have you sign and initial on all of our safety rules so that you can be the trained operator. Also silly string, gum, hair dyes, sticky substances, candy, and food or drink stains will be subject to $150.00 cleaning charge. Silly string damage is permanent and will be charged the entire cost of the unit, please be aware of this, once paid you will own the unit.",
+  },
+  {
+    title: "Do you have insurance?",
+    text:
+      "Yes. We carry a liability insurance policy. Please note that all individuals and/or companies that rent from Southern Ohio Inflatables are required to sign a rental agreement prior to setting up the equipment. We cannot set up the inflatables until the rental agreement has been signed. With a 7 day notification you can be added as an additional insured to our policy for $50",
+  },
+  {
+    title: "What should I do if I lose power or have another problem?",
+    text:
+      " Immediately call us at 937-217-3440 with any concerns regarding personnel or equipment. In case of emergency, dial 911. Interrupted power is the primary source of inflatable failures. Should the Blowers provided to you stop working, immediately remove the children playing in or on it. Then check the circuit breaker to see if it has tripped. If the circuit breaker has tripped, try to identify and isolate any other equipment from the circuit before resetting the breake",
+  },
+  {
+    title: "What is your cancellation policy?",
+    text:
+      "You may cancel your reservation, however, the deposit is non-refundable. *If you cancel within 0-3 days of your reservation, your deposit is non-refundable. *If you cancel 4 or more days prior to your reservation, we will apply the deposit to a new date within the same calendar year. *Once the delivery truck leaves the warehouse, you are responsible for payment in full, NO EXCEPTIONS. *Cancellations due to weather are determined the morning of your event before we deliver. If you decide to cancel due to poor weather conditions, your deposit would be refunded.",
+  },
+  {
+    title: "What is your weather policy?",
+    text:
+      "Inflatables cannot be used if there are winds of 20 mph (15 mph for some items) or more, and cannot be used in the rain. It would not be safe for the children. The morning of your event, we monitor the weather using www.weather.com. If there is a 50% chance or more for rain or a prediction of winds 20mph or above, during the time of your event, you may decide to cancel the day of your event. If you decide to cancel your event, it must be prior to delivery. You will be able to get a refund of your deposit/amount paid, or it may be applied to your future rental. You MUST call us between 5am-7am, the morning of your event, to cancel and to receive your refund/credit. Please note, we MUST keep safety in mind at all times, so Southern Ohio Inflatables reserves the right to cancel your rental due to inclement weather at any time.",
+    rain:
+      "-Rain- If there is a 50% or greater chance for rain during the scheduled time of your event, you may cancel or reschedule your event between 5am-7am the morning of the scheduled delivery. However, if the forecast is calling for scattered showers, we recommend you continue with your scheduled delivery. If the forecast is for continuous rain throughout the day, we recommend you cancel or reschedule your event.",
+    wind:
+      "-Wind- Inflatables cannot be used in winds exceeding 20mph (15 mph for some items). If the forecast is for high winds, we will call to cancel/reschedule your rental. You will be able to receive a full refund if your event is cancelled, or use it towards a future date in the same calendar year.",
+    temp:
+      "-Temperature- All inflatables can be used in a wide range of temperatures. If the temperature is expected to fall below 45 degrees, at any time during your event, we will call the morning of your event to cancel/reschedule your delivery. You will be able to receive a full refund or use it toward a future date in the same calendar year. Keep in mind, any temperature above this, the children’s activity will keep them very warm, and we recommend you continue as scheduled. Deposits will not be refunded for temperature cancellations above 45 degrees.",
+  },
+  {
+    title: "Do I need to clean the unit?",
+    text:
+      "You, the customer, will need to inspect the inflatable to make sure that there will only be a reasonable amount of cleaning done by our staff or you will forfeit your security deposit for additional cleaning costs.",
+  },
+  {
+    section: "Tents",
+    title: "Do you deliver the tents?",
+    text: "Our same delivery policy for inflatables do apply but we require a minimum order of $160. If your order does not meet that, you can choose to pay for delivery ($1 a mile each way).",
+  },
+  {
+    title:"What surfaces do you set up on?",
+    text:"This depends on the tent. Pole tents are designed to be anchored into the ground. We can set them up on dirt or asphalt (we use plugs to fill the holes in asphalt after removal). Our frame tents can be set up on any surface with the use of water barrels for weights. Water barrels are an extra $10 each",
+  },
+  {
+    title:"What size tent do I need?",
+    text:"Many factors go into determining the proper size of tent needed. First you need to determine the number of people you want to accommodate in the tent, as well as the style of seating.",
+  },
+  {
+    title:"Do you allow customers to BBQ under tents?",
+    text:"Customers shall not cook under or near tents. Customers are responsible for costs incurred for damage to and/or cleaning costs of tent tops due to cooking under or near tents.",
+  },
+  {
+    title:"Do you allow customers to put up their own tent?",
+    text:"Our quick pop up tents are the only items that we have available for customer installation.",
+  },
+  {
+    title:"Do your tents have sides?",
+    text:"All of our tents are rented without sides and at this time, it is not an available option.",
+  },
+  {
+    title:"What about inclement weather?",
+    text:"Inclement weather could potentially affect your event. While our tents and their components are considered to be in good working condition and may be engineered to meet certain standards, tents/canopies are temporary structures. Strong winds, snow loads, lightning and the like, may create situations that could threaten a tent’s viability.",
+    rain:"Customers should be aware of severe weather conditions and have a plan to evacuate a tent if such conditions manifest. If severe weather threatens a tent(s), it is the customer’s responsibility to evacuate the tent(s). Failure to evacuate the tent site in severe weather can result in serious injuries or fatalities.",
+    wind:"If lightning is seen or thunder is heard, immediately evacuate the tent until the chance of a lightning strike is no longer present. Poles and frames are excellent conductors and provide the grounding that lightning requires.",
+    temp:"In the event of projected or actual conditions, Southern Ohio Inflatables may dismantle or remove any equipment to ensure safety of persons or property. Tents are not guaranteed water or leak proof.",
+  },
+  {
+    title:"What preparations do I need to make before delivery?",
+    text:"Please make sure erections area of the tent has an 8’ clearance away from power lines. Prior to delivery and pick up, please remove pet feces in and around the area where equipment is being setup and used. Please prevent pets from urinating on equipment while it is set up on your property. We recommend you mow your yard 1-2 days in advance of setup",
+  },
+  {
+    section:"Concessions, Tables, and Chairs",
+    title:"Do you deliver concessions, tables, and chairs?",
+    text:"Our same delivery policy for inflatables do apply but we require a minimum order of $160. If your order does not meet that, you can choose to pay for delivery ($1 a mile each way) or you can choose to pick up your items",
+  },
+  {
+    title:"Do I have to clean the equipment",
+    text:"We usually go off of common curtesy on this one. If excessive cleaning or any negligent damage is done, you will be responsible for repairs or cleaning fees.",
+  },
+  {
+    title:"Do I have to buy the supplies with the concession equipment?",
+    text:"You will need a sturdy and clean surface to set them on. You will also need 1 separate 15 amp circuit to operate them.",
+  },
+];
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,31 +1,45 @@
-import React from "react"
+import React from "react";
 
-import Title from "../constants/Title"
-import Questions from '../constants/Questions'
-import styles from '../css/faq.module.css'
+import Title from "../constants/Title";
+import Questions from "../constants/Questions";
+import styles from "../css/faq.module.css";
 
 const Faq = () => {
   return (
     <div>
       <div className={styles.faqTitleWrapper}>
-       <Title title="Frequently Asked Questions" />
-       </div>
+        <Title title="Frequently Asked Questions" />
+      </div>
       <div className={styles.faqWrapper}>
         <div className={styles.faqSecondary}>
-      <div className={styles.faqinfoWrapper}>
-          {Questions.map((item, index) => {
-            return (
-              <article key={index}>
-                <h1>{item.section}</h1>
-                <h3>{item.title}</h3>
-                <p>{item.text}</p>
-              </article>
-            )
-          })}
+          <div className={styles.faqinfoWrapper}>
+            {Questions.map((item, index) => {
+              return (
+                <article key={index}>
+                  <h1>{item.section}</h1>
+                  <h3>{item.title}</h3>
+                  <p>{item.text}</p>
+                  <p>{item.rain}</p>
+                  <p>{item.wind}</p>
+                  <p>{item.temp}</p>
+                </article>
+              );
+            })}
+            <h5>
+              Still have a question? Call or Write us for more information.
+              </h5>
+              <h5>
+                <a href="mailto: Southern.Ohio.Inflatables@gmail.com? subject=subject text">
+                  e-mail: Southern.Ohio.Inflatables@gmail.com
+                </a>
+                </h5>
+                <h5>
+                <a href="tel:937-217-3440">Phone: 937-217-3440</a>
+                </h5>
           </div>
         </div>
-        </div>
-        </div>
-  )
-}
-export default Faq
+      </div>
+    </div>
+  );
+};
+export default Faq;
