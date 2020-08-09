bbe90c9698a4039066f4cc5f2a5d9eb90096ecb0
Ashley <mayashley052@gmail.com>
2020-08-09 19:20:25 -0400

    image gallery fix

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Slide } from "react-slideshow-image"
 import styles from "../css/slideShow.module.css"
-const img1 = require("../images/slideBryan.png")
-const img2 = require("../images/waterSlide.png")
+const img1 = require("../images/photos4.png")
+const img2 = require("../images/photos3.png")
 const img3 = require("../images/aboutImg.jpg")
-const img4 = require('../images/bounceCastle2.jpg');
-
-const slideImages = [img1, img2, img3, img4]
+const img4 = require('../images/photos2.png');
+const img5 = require('../images/slideBryan.png');
+const slideImages = [img1, img2, img3, img4, img5]
 
 const properties = {
   duration: 5000,
@@ -42,6 +42,11 @@ const SlideShow = () => {
         </div>
         <div className={styles.eachSlide}>
           <div style={{ backgroundImage: `url(${slideImages[3]})` }}>
+            {/* <span>Slide 3</span> */}
+          </div>
+        </div>
+        <div className={styles.eachSlide}>
+          <div style={{ backgroundImage: `url(${slideImages[4]})` }}>
             {/* <span>Slide 3</span> */}
           </div>
         </div>
